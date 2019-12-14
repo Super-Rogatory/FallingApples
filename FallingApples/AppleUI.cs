ea08afb3699c60fe0f36bb2fe28fba27661fb5cd
super-rogatory <chukwudi10@gmail.com>
2019-12-13 18:32:50 -0800

    Prototype

--- FallingApples/AppleUI.cs
+++ FallingApples/AppleUI.cs
@@ -11,12 +11,14 @@ using System;
 using System.Windows;
 using System.Drawing;
 using System.Windows.Forms;
-using System.Windows.Input;
 namespace FallingApples
 {
     public class AppleUI : Form
     {
-
+        protected static int losses = 3; 
+        protected static int wins = 0;
+        protected string lossestext = String.Format("LIVES: {0}", losses.ToString());
+        protected string winstext = String.Format("WINS: {0}", wins.ToString());
         protected static int width = 1920;
         protected const int radius = 16;
         protected const int X_OFFSET = 1; //x appears to be offset by 1
@@ -25,6 +27,7 @@ namespace FallingApples
         protected static Bitmap bitmap_brown = new Bitmap(width, 860);
         protected static Bitmap bitmap_controls = new Bitmap(width, 1080);
         protected static Bitmap bitmap_apple = new Bitmap(radius, radius);
+        protected Label lossescounter, winscounter;
         PictureBox bluepicture = new PictureBox();
         PictureBox brownpicture = new PictureBox();
         PictureBox controlspicture = new PictureBox();
@@ -32,6 +35,7 @@ namespace FallingApples
         Random randomint = new Random();
         protected bool isOn;
         protected bool sendAnother;
+        protected bool addloss;
         protected static double x_position = 0;
         protected static double y_position = 0;
         protected const double delta = 0.05;
@@ -44,55 +48,28 @@ namespace FallingApples
             Size = new Size(1920, 1080);
             BallClock.Elapsed += new System.Timers.ElapsedEventHandler(GravityTick); //while the clock is on, it will update ball position
             GraphicClock.Elapsed += new System.Timers.ElapsedEventHandler(GravityTick);
-            /*
-            bluepicture.Location = new Point(0, 0);
-            bluepicture.Size = new Size(1920, 600);
-            Controls.Add(bluepicture);
-            bluepicture.Image = bitmap_blue;
-            bluepicture.Paint += new PaintEventHandler(Paintbitmap_blue);
-
-            brownpicture.Location = new Point(0, 600);
-            brownpicture.Size = new Size(1920, 860);
-            brownpicture.Image = bitmap_apple;
-            Controls.Add(brownpicture);
-            brownpicture.Paint += new PaintEventHandler(Paintbitmap_brown);
-            */
             BitMapBackGround(); //to create the static bitmaps for the background and the apple. Wanted to avoid invalidation on these paintings
             RenderButtons();
         }
-        /*
-        void Paintbitmap_blue(object sender, PaintEventArgs e)
-        {
-            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
-            using (Graphics gfx = Graphics.FromImage(bitmap_blue))
-            using (SolidBrush brush = new SolidBrush(Color.SkyBlue))
-            {
-                gfx.FillRectangle(brush, new Rectangle(0, 0, width, 600));
-            }
-        }
-        void Paintbitmap_brown(object sender, PaintEventArgs e)
-        {
-            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
-            using (Graphics gfx = Graphics.FromImage(bitmap_brown))
-            using (SolidBrush brush = new SolidBrush(Color.SaddleBrown))
-            {
-                gfx.FillRectangle(brush, new Rectangle(0, 600, width, 860));
-            }
-        }
-        */
+      
         protected void RenderButtons()
         {
             start = new Button { Text = "Start", Size = new Size(50, 30), Location = new Point(180, 900), BackColor = Color.GhostWhite };
             pause = new Button { Text = "Pause", Size = new Size(50, 30), Location = new Point(250, 900), BackColor = Color.GhostWhite };
             resume = new Button { Text = "Resume", Size = new Size(150, 60), Location = new Point(875, 900), BackColor = Color.Green, ForeColor = Color.GhostWhite };
             exit = new Button { Text = "Exit the Game", Size = new Size(100, 60), Location = new Point(1600, 900), BackColor = Color.Red, ForeColor = Color.White };
+            lossescounter = new Label { Text = lossestext, Size = new Size(60, 20), Location = new Point(600, 900), BackColor = Color.GhostWhite, Font = new Font(DefaultFont, FontStyle.Bold) };
+            winscounter = new Label { Text = winstext, Size = new Size(55, 20), Location = new Point(700, 900), BackColor = Color.GhostWhite, Font = new Font(DefaultFont, FontStyle.Bold) };
             Controls.Add(start);
             Controls.Add(pause);
+            Controls.Add(lossescounter);
+            Controls.Add(winscounter);
             start.Click += Start;
             pause.Click += PauseMotion;
             resume.Click += ResumeMotion;
             exit.Click += EndApplication;
             MouseClick += MouseClickA; //calls mousePressed whenever the mouse is click
+
         }
 
         protected static void BitMapBackGround()
@@ -125,19 +102,26 @@ namespace FallingApples
             double x = Math.Sqrt(Math.Pow(Convert.ToInt32(x_position) - e.Location.X, 2));
             double y = Math.Sqrt(Math.Pow(Convert.ToInt32(y_position) - e.Location.Y, 2));
             double distance = x + y;
-            if (Convert.ToInt32(distance) <= (radius + 8)) //give or take some pixels
+            if (y_position > 600) //TODO:
             {
-                Console.WriteLine("Perfect Click!");
+                if (Convert.ToInt32(distance) <= (radius + 8)) 
+                {
+                    // GOOD, gain one point
+                    wins = wins + 1;
+                    winscounter.Text = String.Format("WINS: {0}", wins.ToString());
+                    sendAnother = true; 
+                }
             }
         }
         protected override void OnPaint(PaintEventArgs e)
         {
 
             // drawing the images
-            /*e.Graphics.DrawImage(bitmap_blue, 0, 0, width, 600);
+
+            e.Graphics.DrawImage(bitmap_blue, 0, 0, width, 600);
             e.Graphics.DrawImage(bitmap_brown, 0, 0, width, 860);
             e.Graphics.DrawImage(bitmap_controls, 0, 0, width, 1080);
-            */
+
             e.Graphics.DrawImage(bitmap_apple, Convert.ToInt32(x_position), Convert.ToInt32(y_position), radius, radius); //bitmap for the ball
 
             if (isOn)
@@ -150,9 +134,29 @@ namespace FallingApples
                 case true:
                     if (sendAnother)
                     {
-                        //Add to counter
+                        if (losses.Equals(0))
+                        {
+                            // TODO: GAME PAUSED, CLICK RESUME TO CONTINUE
+                            isOn = false;
+                            GraphicClock.Enabled = false;
+                            BallClock.Enabled = false;
+                        }
+                        if (wins.Equals(10))
+                        {
+                            // TODO: GAME PAUSED, CLICK RESUME TO CONTINUE
+                            isOn = false;
+                            GraphicClock.Enabled = false;
+                            BallClock.Enabled = false;
+                        }
+                        x_position = randomint.Next(16, 1904);
                         y_position = 0;
                         sendAnother = false;
+                    }
+                    else if (addloss)
+                    {
+                        losses = losses - 1;
+                        lossescounter.Text = String.Format("LIVES: {0}", losses.ToString());
+                        addloss = false;
                     }
                     break;
                 case false: //TODO: Occurs only when loop count is at 10. Display Exit message
@@ -165,18 +169,20 @@ namespace FallingApples
         }
         protected void StartingGraphicClock()
         {
-            GraphicClock.Interval = 1.0;
+            GraphicClock.Interval = 0.00001;
         }
         protected void StartingBallClock()
         {
-            BallClock.Interval = Convert.ToInt32((1000.0 / 17.0));
+            BallClock.Interval = 0.00001;
         }
         protected void GravityTick(object sender, EventArgs events)
         {
             y_position += delta;
-            if ((int)Math.Round(y_position) == 860)
+            if ((int)Math.Round(y_position) == 844)
             {
+                // BAD, Lose one point
                 sendAnother = true;
+                addloss = true;
             }
             Invalidate();
         }
@@ -185,14 +191,18 @@ namespace FallingApples
             isOn = true;
             GraphicClock.Enabled = true;
             BallClock.Enabled = true;
+            losses = 3;
+            lossescounter.Text = String.Format("LIVES: {0}", losses.ToString());
+            x_position = randomint.Next(16, 1904);
         }
         protected void ResumeMotion(object sender, EventArgs events)
         {
+            isOn = true;
+            GraphicClock.Enabled = true;
+            BallClock.Enabled = true;
             Controls.Add(start);
             Controls.Add(pause);
             Controls.Remove(resume);
-            GraphicClock.Enabled = true;
-            BallClock.Enabled = true;
         }
         protected void PauseMotion(object sender, EventArgs events)
         {
